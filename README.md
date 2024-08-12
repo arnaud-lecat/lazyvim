415decbb4e6cfbff603fe5b29da9c0e6d5eb0565
Arnaud Lecat <alecat@groupe-audeo.com>
2024-08-12 14:25:29 +0200

    Init my lazyvim

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# 💤 LazyVim
+# Arnaud's 💤 LazyVim
+
+## About LazyVim
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,7 +11,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.ui.alpha" },
     { import = "plugins" },
   },
   defaults = {
